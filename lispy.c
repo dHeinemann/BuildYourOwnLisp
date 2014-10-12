3d294184a1ee589e1468791f505dd75994c48344
David Heinemann <dheinemann@fastmail.fm>
2014-10-12 13:58:07 +1000

    Implemented exit command.

--- lispy.c
+++ lispy.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 static char input[2048];
 
 int main(int argc, char** argv)
 {
     puts("Lispy version 0.0.0.01");
-    puts("Press Ctrl+c to exit.\n");
+    puts("Enter 'exit' to quit.\n");
 
     while (1)
     {
         fputs("lispy> ", stdout);
         fgets(input, 2048, stdin);
+        if (strncmp(input, "exit", 4) == 0)
+        {
+            break;
+        }
         printf("No, you're a %s", input);
     }
 
